4caa41f81f9b321701a5615a1e0edeb9b65eb993
david-soto-m <davidsotomarchena@protonmail.com>
2022-03-16 12:21:15 +0100

    array is better as list

--- plotter.m
+++ plotter.m
@@ -5,9 +5,9 @@ sec_str = "Segundos";
 deg_change = 30;
 out=sim("parte_1.slx");
 datas={
-    out.P_alphabet,"Potencia \alpha \beta", ["Watios", sec_str],[], [];
-    out.V_alphabet,"Tensión \alpha \beta", ["Voltios", sec_str],[], [];
-    out.I_alphabet,"Intensidad \alpha \beta", ["Amperios", sec_str],[], [];
+    out.P_alphabet,"Potencia \alpha \beta", {"Watios", sec_str},[], [];
+    out.V_alphabet,"Tensión \alpha \beta", {"Voltios", sec_str},[], [];
+    out.I_alphabet,"Intensidad \alpha \beta", {"Amperios", sec_str},[], [];
 };
 pl.deal_datas(datas, [3,1], "alphabet_inc");
 
